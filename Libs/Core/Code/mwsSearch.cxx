468d0508cd679704fa4935352aaf18ed43e6df47
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2012-02-02 16:10:24 -0500

    ENH: refs #0245. Get search restults from Midas3 webapi which calls the Midas3 search component.

--- Libs/Core/Code/mwsSearch.cxx
+++ Libs/Core/Code/mwsSearch.cxx
@@ -21,6 +21,9 @@
 #include "mdoCollection.h"
 #include "mdoItem.h"
 #include "mdoBitstream.h"
+#include "m3doFolder.h"
+#include "m3doCommunity.h"
+#include "m3doItem.h"
 #include "midasUtils.h"
 #include <stdio.h>
 #include <string.h>
@@ -57,59 +60,100 @@ public:
 
     QScriptEngine engine;
     QScriptValue  data = engine.evaluate(response).property("data");
+    if( SERVER_IS_MIDAS3 )
+	  {
+      if( data.property("results").isArray() )
+        {
+        QScriptValueIterator rslts(data.property("results") );
+        while( rslts.hasNext() )
+          {
+          rslts.next();
+		  std::string rsltType = rslts.value().property("resultType").toString().toStdString();
+		  if( rsltType.compare("item") == 0 )
+		    {
+			m3do::Item* item = new m3do::Item();
+            item->SetId(rslts.value().property("item_id").toInt32() );
+            item->SetUuid(rslts.value().property("uuid").toString().toStdString().c_str() );
+            item->SetName(rslts.value().property("name").toString().toStdString().c_str() );
+            m_Results->push_back(item);
+		    }
+		  else if( rsltType.compare("folder") == 0 )
+		    {
+            m3do::Folder* fldr = new m3do::Folder();
+			fldr->SetId(rslts.value().property("folder_id").toInt32() );
+            fldr->SetUuid(rslts.value().property("uuid").toString().toStdString().c_str() );
+            fldr->SetName(rslts.value().property("name").toString().toStdString().c_str() );
+            m_Results->push_back(fldr);
+		    }
+		  else if( rsltType.compare("community") == 0 )
+		    {
+		    m3do::Community* comm = new m3do::Community();
+			comm->SetFolderId(rslts.value().property("folder_id").toInt32() );
+            comm->SetUuid(rslts.value().property("uuid").toString().toStdString().c_str() );
+            comm->SetName(rslts.value().property("name").toString().toStdString().c_str() );
+            m_Results->push_back(comm);
+		    }  
+		  // TODO:  else if( rsltType.compare("user") == 0 )
+          }
+        }
+	  }
+	else
+	  {
+       if( data.property("communities").isArray() )
+        {
+        QScriptValueIterator comms(data.property("communities") );
+        while( comms.hasNext() )
+          {
+          comms.next();
+          mdo::Community* comm = new mdo::Community();
+          comm->SetId(comms.value().property("community_id").toInt32() );
+          comm->SetUuid(comms.value().property("uuid").toString().toStdString().c_str() );
+          comm->SetName(comms.value().property("community_name").toString().toStdString().c_str() );
+          m_Results->push_back(comm);
+          }
+        }
+      if( data.property("collections").isArray() )
+        {
+        QScriptValueIterator colls(data.property("collections") );
+        while( colls.hasNext() )
+          {
+          colls.next();
+          mdo::Collection* coll = new mdo::Collection();
+          coll->SetId(colls.value().property("collection_id").toInt32() );
+          coll->SetUuid(colls.value().property("uuid").toString().toStdString().c_str() );
+          coll->SetName(colls.value().property("collection_name").toString().toStdString().c_str() );
+          m_Results->push_back(coll);
+          }
+        }
+      if( data.property("items").isArray() )
+        {
+        QScriptValueIterator items(data.property("items") );
+        while( items.hasNext() )
+          {
+          items.next();
+          mdo::Item* item = new mdo::Item();
+          item->SetId(items.value().property("item_id").toInt32() );
+          item->SetUuid(items.value().property("uuid").toString().toStdString().c_str() );
+          item->SetTitle(items.value().property("item_name").toString().toStdString().c_str() );
+          m_Results->push_back(item);
+          }
+        }
+      if( data.property("bitstreams").isArray() )
+        {
+        QScriptValueIterator bitstreams(data.property("bitstreams") );
+        while( bitstreams.hasNext() )
+          {
+          bitstreams.next();
+          mdo::Bitstream* bitstream = new mdo::Bitstream();
+          bitstream->SetId(bitstreams.value().property("bitstream_id").toInt32() );
+          bitstream->SetUuid(bitstreams.value().property("uuid").toString().toStdString().c_str() );
+          bitstream->SetName(bitstreams.value().property("bitstream_name").toString().toStdString().c_str() );
+          m_Results->push_back(bitstream);
+          }
+        }
+	  }
 
-    if( data.property("communities").isArray() )
-      {
-      QScriptValueIterator comms(data.property("communities") );
-      while( comms.hasNext() )
-        {
-        comms.next();
-        mdo::Community* comm = new mdo::Community();
-        comm->SetId(comms.value().property("community_id").toInt32() );
-        comm->SetUuid(comms.value().property("uuid").toString().toStdString().c_str() );
-        comm->SetName(comms.value().property("community_name").toString().toStdString().c_str() );
-        m_Results->push_back(comm);
-        }
-      }
-    if( data.property("collections").isArray() )
-      {
-      QScriptValueIterator colls(data.property("collections") );
-      while( colls.hasNext() )
-        {
-        colls.next();
-        mdo::Collection* coll = new mdo::Collection();
-        coll->SetId(colls.value().property("collection_id").toInt32() );
-        coll->SetUuid(colls.value().property("uuid").toString().toStdString().c_str() );
-        coll->SetName(colls.value().property("collection_name").toString().toStdString().c_str() );
-        m_Results->push_back(coll);
-        }
-      }
-    if( data.property("items").isArray() )
-      {
-      QScriptValueIterator items(data.property("items") );
-      while( items.hasNext() )
-        {
-        items.next();
-        mdo::Item* item = new mdo::Item();
-        item->SetId(items.value().property("item_id").toInt32() );
-        item->SetUuid(items.value().property("uuid").toString().toStdString().c_str() );
-        item->SetTitle(items.value().property("item_name").toString().toStdString().c_str() );
-        m_Results->push_back(item);
-        }
-      }
-    if( data.property("bitstreams").isArray() )
-      {
-      QScriptValueIterator bitstreams(data.property("bitstreams") );
-      while( bitstreams.hasNext() )
-        {
-        bitstreams.next();
-        mdo::Bitstream* bitstream = new mdo::Bitstream();
-        bitstream->SetId(bitstreams.value().property("bitstream_id").toInt32() );
-        bitstream->SetUuid(bitstreams.value().property("uuid").toString().toStdString().c_str() );
-        bitstream->SetName(bitstreams.value().property("bitstream_name").toString().toStdString().c_str() );
-        m_Results->push_back(bitstream);
-        }
-      }
+    
     return true;
   }
 
@@ -129,7 +173,7 @@ std::vector<mdo::Object *> Search::SearchServer(std::vector<std::string> tokens)
   SearchResponseParser       parser;
   parser.SetObject(&results);
 
-  std::string fields = "midas.resources.search&search=";
+  std::string fields = "midas.resource.search&search=";
 
   bool space = false;
   for( std::vector<std::string>::iterator i = tokens.begin();
