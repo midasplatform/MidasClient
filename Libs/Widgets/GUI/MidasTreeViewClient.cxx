c01d9ed6e26478b5995afc3e0d5a46cf785bede5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-06 01:35:53 -0500

    BUG: Fix signal/slot connections for Midas2 client/server treeView
    Apply fixes simular to 19f29b4 (ENH: refs #0295. Signal/slot connection
    fixes) but for Midas2.
    
    It addresses the following runtime errors:
    
      Object::connect: No such slot MidasTreeModelClient::itemCollapsed(const QModelIndex &)
      Object::connect: No such slot MidasTreeModelClient::itemExpanded(const QModelIndex &)
      Object::connect: No such signal MidasTreeModelClient::expand(const QModelIndex &)
      Object::connect: No such slot MidasTreeViewClient::updateSelection(const QItemSelection &, const QItemSelection & )
      Object::connect: No such slot MidasTreeModelServer::itemCollapsed(const QModelIndex &)
      Object::connect: No such slot MidasTreeModelServer::itemExpanded(const QModelIndex &)
      Object::connect: No such signal MidasTreeModelServer::expand(const QModelIndex &)
      Object::connect: No such signal MidasTreeModelServer::fetchedMore()
      Object::connect: No such slot MidasTreeViewServer::updateSelection(const QItemSelection &, const QItemSelection & )

--- Libs/Widgets/GUI/MidasTreeViewClient.cxx
+++ Libs/Widgets/GUI/MidasTreeViewClient.cxx
@@ -48,17 +48,17 @@ MidasTreeViewClient::MidasTreeViewClient(QWidget* parent)
   this->setSelectionMode(QTreeView::SingleSelection);
 
   connect(this, SIGNAL( collapsed(const QModelIndex &) ),
-          this->model(), SLOT(itemCollapsed(const QModelIndex &) ) );
+          this->model(), SLOT(ItemCollapsed(const QModelIndex &) ) );
   connect(this, SIGNAL( expanded(const QModelIndex &) ),
-          this->model(), SLOT(itemExpanded(const QModelIndex &) ) );
-  connect(m_Model, SIGNAL( expand(const QModelIndex &) ),
+          this->model(), SLOT(ItemCollapsed(const QModelIndex &) ) );
+  connect(m_Model, SIGNAL( Expand(const QModelIndex &) ),
           this, SLOT( expand(const QModelIndex &) ) );
 
   // define action to be triggered when tree item is selected
   QItemSelectionModel* itemSelectionModel = this->selectionModel();
   connect(itemSelectionModel,
           SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &) ),
-          this, SLOT(updateSelection(const QItemSelection &, const QItemSelection & ) ) );
+          this, SLOT(UpdateSelection(const QItemSelection &, const QItemSelection & ) ) );
 
   m_MimeType = "MIDAS/client_resource";
   m_AcceptMimeType = "MIDAS/server_resource";
